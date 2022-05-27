f2ed4f3fe3082856bd169ce9cf4e77895e942fd0
amellalalex <github@alexamellal.com>
2022-05-27 11:48:01 -0300

    added error insertion encoding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
@@ -68,6 +69,41 @@ fn decode_file(path_in: &str, path_out: &str) -> AllResult {
     Ok(())
 }
 
+fn encode_file_with_err(path_in: &str, path_out: &str) -> AllResult {
+    let encoder = Encoder::new(ENCODED_BLOCK_SIZE - DATA_BLOCK_SIZE);
+
+    let file_in = File::open(path_in)?;
+    let file_out = File::create(path_out)?;
+
+    let mut filebuf_in = BufReader::new(file_in);
+    let mut filebuf_out = BufWriter::new(file_out);
+
+    let mut buf: [u8; DATA_BLOCK_SIZE] = [0; DATA_BLOCK_SIZE];
+    while let Ok(n) = filebuf_in.read(&mut buf) {
+        // Pad EOF missing bytes
+        if n == 0 {
+            break;
+        } else if n < DATA_BLOCK_SIZE {
+            for x in buf[n..DATA_BLOCK_SIZE - 1].iter_mut() {
+                *x = 0;
+            }
+        }
+
+        // Encode
+        let mut encoded = encoder.encode(&buf);
+
+        // Add errors
+        let mut rng = rand::thread_rng();
+        for x in encoded.iter_mut().take(2) {
+            *x = rng.gen::<u8>();
+        }
+
+        filebuf_out.write(encoded.deref())?;
+    }
+
+    Ok(())
+}
+
 fn main() -> AllResult {
     let args: Vec<String> = env::args().collect();
     if args.len() != 4 {
@@ -84,6 +120,8 @@ fn main() -> AllResult {
         encode_file(path_in, path_out)?;
     } else if op == "decode" {
         decode_file(path_in, path_out)?;
+    } else if op == "errcode" {
+        encode_file_with_err(path_in, path_out)?;
     } else {
         println!("{op} is not a valid op, try 'encode' or 'decode' instead");
     }
